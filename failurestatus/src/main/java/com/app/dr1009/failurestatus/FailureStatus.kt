57c39ef2e0e17680178b86c62572a81ecbbde2d6
Koji Wakamiya <koji.wakamiya@studyplus.jp>
2019-06-06 19:51:34 +0900

    Add JavaDoc

--- failurestatus/src/main/java/com/app/dr1009/failurestatus/FailureStatus.kt
+++ failurestatus/src/main/java/com/app/dr1009/failurestatus/FailureStatus.kt
@@ -1,15 +1,27 @@
 package com.app.dr1009.failurestatus
 
+/**
+ * Result status.
+ */
 enum class Status {
     SUCCESS,
     ERROR,
     LOADING
 }
 
+/**
+ * FailureStatus is a class that encapsulates a Throwable.
+ *
+ * @param message Error message for User.
+ * @param throwable Cause of the error.
+ */
 data class FailureStatus(val message: String? = null, val throwable: Throwable? = null) {
     constructor(message: String?) : this(message = message, throwable = null)
     constructor(t: Throwable?) : this(message = null, throwable = t)
 
+    /**
+     * Get an error message from the cause. If the message has already been set, the function is aborted.
+     */
     fun createMessage(throwableFunc: (throwable: Throwable?) -> String): String =
         if (!message.isNullOrEmpty()) {
             message
@@ -18,10 +30,13 @@ data class FailureStatus(val message: String? = null, val throwable: Throwable? 
         }
 }
 
+/**
+ * The state of network access.
+ */
 sealed class NetworkState(val status: Status) {
-    object LOADED: NetworkState(Status.SUCCESS)
-    object LOADING: NetworkState(Status.LOADING)
-    data class ERROR(val error: FailureStatus): NetworkState(Status.ERROR) {
+    object LOADED : NetworkState(Status.SUCCESS)
+    object LOADING : NetworkState(Status.LOADING)
+    data class ERROR(val error: FailureStatus) : NetworkState(Status.ERROR) {
         constructor(throwable: Throwable?) : this(FailureStatus(throwable))
         constructor(message: String?) : this(FailureStatus(message))
     }
--- failurestatus/src/main/java/com/app/dr1009/failurestatus/FailureStatusExtension.kt
+++ failurestatus/src/main/java/com/app/dr1009/failurestatus/FailureStatusExtension.kt
@@ -1,9 +1,9 @@
 package com.app.dr1009.failurestatus
 
 import android.app.Activity
-import com.google.android.material.snackbar.Snackbar
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 
 fun Activity.networkErrorBar(
     anchor: View,
@@ -22,16 +22,16 @@ fun Activity.networkErrorBar(
     bar.show()
 }
 
-fun androidx.fragment.app.Fragment.networkErrorBar(
+fun Fragment.networkErrorBar(
     anchor: View,
     status: FailureStatus,
     message: (throwable: Throwable?) -> String,
-    duration: Int = com.google.android.material.snackbar.Snackbar.LENGTH_SHORT,
+    duration: Int = Snackbar.LENGTH_SHORT,
     isShowAction: Boolean = true,
     actionMessage: (throwable: Throwable?) -> String = { getString(android.R.string.ok) },
     action: (throwable: Throwable?) -> Unit = {}
 ) {
-    val bar = com.google.android.material.snackbar.Snackbar.make(anchor, status.createMessage(message), duration)
+    val bar = Snackbar.make(anchor, status.createMessage(message), duration)
     if (isShowAction) {
         bar.setAction(actionMessage(status.throwable)) { action(status.throwable) }
     }
